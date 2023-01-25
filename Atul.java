520fd1a84ef3201d7e3cac23b653413c13f28c56
atul-xt <atul.xtt@gmail.com>
2023-01-25 14:35:15 +0530

    First Commit

---
+++ Atul.java
@@ -0,0 +1,8 @@
+class Atul
+{
+	public static void main(String args[])
+ {
+	System.out.println("Hello Atul");
+ }
+
+}
---
+++ Atul2.java
@@ -0,0 +1,8 @@
+class Atul2
+{
+	public static void main(String args[])
+ {
+	System.out.println("Hello Atul");
+ }
+
+}
